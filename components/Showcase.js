db55efda1b4b0e0cb9473e9ba2d232c51efa6ad8
Maazasim1 <mazzasimq1@gmail.com>
2022-09-15 00:42:20 +0500

    finished showcase animation

--- components/Showcase.js
+++ components/Showcase.js
@@ -14,7 +14,7 @@ export default function Showcase(props) {
 
       <div className="grid grid-cols-1 row-start-1 col-start-1">
 
-        <img src='/images/mockups.png'  height='250px' width='550px' quality={50} className='max-w-[95%]  z-40 row-start-1 col-start-1' />
+        <img src='/images/mockups.png'  height='250px' width='550px' className='max-w-[95%]  z-40 row-start-1 col-start-1' />
         <div className="h-[278px] w-[412px] z-30 overflow-hidden row-start-1 col-start-1 mt-5 ml-16 max-h-[90%] max-w-[73%]" >
           <motion.img animate={{ y: [0, -600, 0] }} transition={{ ease: "linear", duration: 50, repeat: Infinity }}  src='/images/elecbrakes.png' height='2300px' width='550px' />
 
@@ -24,7 +24,7 @@ export default function Showcase(props) {
       
       <div className="grid grid-cols-1 row-start-1 col-start-1 z-40 ml-48">
 
-        <img src='/images/mockup_mobile.png' height='150px' width='350px' quality={50} className='z-40 row-start-1 col-start-1' />
+        <img src='/images/mockup_mobile.png' height='150px' width='350px'  className='z-40 row-start-1 col-start-1' />
         <div className="rounded-2xl row-start-1 col-start-1 h-[290px] w-[134px] z-30 overflow-hidden mt-8 ml-[110px]" >
           <motion.img animate={{ y: [0, -600, 0] }} transition={{ ease: "linear", duration: 50, repeat: Infinity }}  src='/images/elecbrakes_mobile.png' height={2300} width={350} />
 
