55b70c988e5523782a36de961658f981175fa063
Maaz Asim <58209878+Maazasim1@users.noreply.github.com>
2022-11-08 21:05:18 +0500

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import {ParallaxProvider} from 'react-scroll-parallax'
@@ -5,10 +6,27 @@ import {ParallaxProvider} from 'react-scroll-parallax'
 
 function MyApp({ Component, pageProps }) {
   return(
+    <>
+    
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"/>
+    <Script
+      id='google-analytics'
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-6HCF5ENS4Y', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+    />
     <ParallaxProvider>
       <Component {...pageProps} />
     </ParallaxProvider>
-    
+    <>
     ) 
 }
 
